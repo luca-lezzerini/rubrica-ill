87fb07036c0cf7d93074e4218b4d0b39991f895c
Giulia2Gargano <giulia2gargano@gmail.com>
2021-02-06 11:29:26 +0100

    primo commit

---
+++ rubrica-ill-server/src/main/java/it/sirfin/rubricaill/RubricaillApplication.java
@@ -0,0 +1,13 @@
+package it.sirfin.rubricaill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubricaillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RubricaillApplication.class, args);
+	}
+
+}
---
+++ rubrica-ill-server/src/test/java/it/sirfin/rubricaill/RubricaillApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sirfin.rubricaill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubricaillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
