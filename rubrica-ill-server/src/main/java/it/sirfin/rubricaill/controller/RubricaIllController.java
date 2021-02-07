8a2a1d814cdc500fee1ec6f6c8482af27b13f849
MatteoPalombo99 <78491909+MatteoPalombo99@users.noreply.github.com>
2021-02-07 10:51:25 +0100

    Creata prima bozza server

--- rubrica-ill-server/src/main/java/it/sirfin/rubricaill/controller/RubricaIllController.java
+++ rubrica-ill-server/src/main/java/it/sirfin/rubricaill/controller/RubricaIllController.java
@@ -5,10 +5,39 @@
  */
 package it.sirfin.rubricaill.controller;
 
-/**
- *
- * @author Admin
- */
+import it.sirfin.rubricaill.dto.RubricaRequest;
+import it.sirfin.rubricaill.dto.RubricaResponse;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
 public class RubricaIllController {
-    
+
+    List<RubricaResponse> rubrica = new ArrayList<>();
+
+    int con = 0;
+
+    @RequestMapping("/rubricagg")
+    @ResponseBody
+    public List<RubricaResponse> rubrica(@RequestBody RubricaRequest dto) {
+        System.out.println("Ricevuto dto " + dto);
+        String n = dto.getNome();
+        String c = dto.getCognome();
+        String t = dto.getTelefono();
+        con++;
+        RubricaResponse r = new RubricaResponse();
+        r.setNome(n);
+        r.setCognome(c);
+        r.setTelefono(t);
+        r.setID(con);
+        rubrica.add(r);
+        System.out.println("Restituisco dto " + rubrica);
+        return rubrica;
+    }
 }
